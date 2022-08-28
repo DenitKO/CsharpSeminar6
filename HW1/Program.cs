ccacfeb98c101039fce20e1b2c52dfbc68072ed2
DenitKO <motovdenis@mail.ru>
2022-08-28 13:42:13 +0300

    added first programm of HomeWorkk

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// Пример  0, 7, 8, -2, -2 -> 2
+
+// Пример  1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите количество вводимых чисел");
+int M = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[M];
+
+Console.WriteLine("Введите числа");
+
+for (int i = 0; i < M; i++)
+{
+  Console.Write($"array[{i}] = ");
+  array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine();
+
+int count_of_positive = 0;
+
+for (int j = 0; j < M; j++)
+{
+    if (array[j] > 0) 
+    {
+    count_of_positive++;
+    }
+}
+
+Console.WriteLine($"Количество положительных чисел = {count_of_positive}");
